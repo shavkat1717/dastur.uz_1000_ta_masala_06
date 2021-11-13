0c708f8447b896876e7a4253e6e50cb3f692f751
shavkat_amirov <84470034+shavkat1717@users.noreply.github.com>
2021-11-13 11:45:58 +0500

    For sikliga oid masalalar
    For sikliga oid masalalar

---
+++ 03_masala.py
@@ -0,0 +1,10 @@
+a=int(input("Istalgan 1-butun sonni kiriting a => "))
+b=int(input("Istalgan 2-butun sonni kiriitng: b => "))
+if b>a:
+    for x in range(b-1,a,-1):
+        print(x)
+    else:
+        print(f"Jami elementlar soni: {b-a-1} ta.")
+        print("Sikl tugadi!")
+else:
+    print("Diqqat! Masala shartiga ko'ra a soni b sonidan kichik bo'lsin!")            
