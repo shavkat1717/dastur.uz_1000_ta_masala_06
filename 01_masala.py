005197370f747615c933cd1675d726c97cc903ab
shavkat_amirov <84470034+shavkat1717@users.noreply.github.com>
2021-11-13 11:44:55 +0500

    For sikliga oid masalalar
    For sikliga oid masalalar

---
+++ 01_masala.py
@@ -0,0 +1,9 @@
+k=int(input("Istalgan k sonini kiriting k => "))
+n=int(input("Necha marta chiqarsin: n => "))
+if n>0:
+    for x in range(n):
+        print(k)
+    else:
+        print("\nSikl tugadi!")
+else:
+    print("n sonini noldan katta kiriting!")            
