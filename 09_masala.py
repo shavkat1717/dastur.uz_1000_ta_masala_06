ab290c427f8cb779c0beede5a63b11299af1b4b2
shavkat_amirov <84470034+shavkat1717@users.noreply.github.com>
2021-11-13 11:48:53 +0500

    For sikliga oid masalalar
    For sikliga oid masalalar

---
+++ 09_masala.py
@@ -0,0 +1,12 @@
+a=int(input("Istalgan 1-butun sonni kiriting a => "))
+b=int(input("Istalgan 2-butun sonni kiriitng: b => "))
+if b>a:
+    s=0
+    for x in range(a,b+1):
+        print(x, end="; ")
+        s=s+(x**2)
+    else:
+        print(f"\n{a} dan {b} gacha bo'lgan butun sonlar kvadratlarining yig'indisi: {s} ga teng ekan!")
+        print("Sikl tugadi!")
+else:
+    print("Diqqat! Masala shartiga ko'ra a soni b sonidan kichik bo'lsin!")            
