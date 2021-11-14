90e9af24122f4878a2aa9874f5d41fe5b0349b68
shavkat_amirov <84470034+shavkat1717@users.noreply.github.com>
2021-11-14 19:03:58 +0500

    For sikliga oid masalalar
    For sikliga oid masalalar

---
+++ 16_masala.py
@@ -0,0 +1,9 @@
+a=float(input("Daraja asosini kiriting: a => "))
+n=int(input("Darajani yani: n > 0 butun sonni kiriting n => "))
+if n>0:
+    s=1
+    for x in range(1,n+1):
+        s=a**x
+        print(s)
+else:
+    print(" n > 0 butun sonni kiriting.")
