a2f4c16de2d125b0e1d6e150f5d0a79125d57189
shavkat_amirov <84470034+shavkat1717@users.noreply.github.com>
2021-11-16 22:46:59 +0500

    For sikliga oid masalalar
    For sikliga oid masalalar

---
+++ 28_masala.py
@@ -0,0 +1,10 @@
+x=float(input(" |x|<1 haqiqiy sonni kiriting x = "))
+n=int(input(" n > 0 darajani kiriting n = "))
+import math
+if n>0 and math.fabs(x)<1:
+    s=x
+    for i in range(1,n+1):
+        s=s+(2*i-1)*pow(x,2*i+1)/(2*i)*(2*i+1)        
+    print(s)
+else:
+    print("Shart: n > 0 va |x| < 1 ga e'tibor bering!")
