248acf35781552707766589b46a5c83579b82528
shavkat_amirov <84470034+shavkat1717@users.noreply.github.com>
2021-11-18 21:56:58 +0500

    Update 38_masala.py
    For sikliga oid masalalar

--- 38_masala.py
+++ 38_masala.py
@@ -1,11 +1,14 @@
 n=int(input("n > 0 butun sonni kiriting: n => "))
 if n>0:
     s=0
+    t=n
+    p=1
     for i in range(1,n+1):
-        print(i)
-        for j in range(n,0,-1):
-            print(j)
-            s=s+pow(i,j)
-            print(s)
+        for j in range(t,0,-1):
+            p=p*i
+        s=s+p 
+        p=1
+        t=t-1
+    print(s)        
 else:
     print("n > 0 shartga e'tibor bering!")
